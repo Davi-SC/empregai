fd93de669f0f7de13ea71ec045e40158954d9aaf
Davi Cruz <cruzdavi001@gmail.com>
2025-05-10 17:27:07 -0300

    fix: cadastro de usuario por tipo(empresa or candidato)

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,5 +1,7 @@
 import bcrypt from "bcryptjs/dist/bcrypt.js";
 import Usuario from "../models/Usuario.js";
+import Candidato from "../models/Candidato.js";
+import Empresa from "../models/Empresa.js";
 import passport from "passport";
 
 class UsuarioController {
@@ -20,32 +22,47 @@ class UsuarioController {
 
   cadastrar = async (req, res) => {
     try {
-      
-      const usuario = await Usuario.findOne({
-        where:{
-          email: req.body.email
-        }
-      })
+      const usuarioExistente = await Usuario.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
 
-      if(usuario) {
-        req.flash('error_msg', 'Este email já foi cadastrado!')
-        return res.redirect('/usuario/cadastrar')
+      if (usuarioExistente) {
+        req.flash("error_msg", "Este email já foi cadastrado!");
+        return res.redirect("/usuario/cadastrar");
       }
 
-      const salt = await bcrypt.genSalt(10)
-      const hashSenha = await bcrypt.hash(req.body.senha, salt)
+      const salt = await bcrypt.genSalt(10);
+      const hashSenha = await bcrypt.hash(req.body.senha, salt);
 
-      let novoUsuario = {
+      const usuario = await Usuario.create({
         nome: req.body.nome,
         email: req.body.email,
         senha: hashSenha,
-        tipo: req.body.tipo
+        tipo: req.body.tipo,
+      });
+
+      if (usuario.tipo === "candidato") {
+        await Candidato.create({
+          usuarioId: usuario.id,
+          areaAtuacao: req.body.areaAtuacao || null,
+          experiencia: req.body.experiencia || null,
+          habilidades: req.body.habilidades || null,
+          localizacao: req.body.localizacao || null,
+        });
+      } else if (usuario.tipo === "empresa") {
+        await Empresa.create({
+          usuarioId: usuario.id,
+          nomeFantasia: req.body.nomeFantasia || null,
+          cnpj: req.body.cnpj || null,
+          setor: req.body.setor || null,
+          localizacao: req.body.localizacaoEmpresa || null,
+        });
       }
 
-      await Usuario.create(novoUsuario).then(() => {
-        req.flash('success_msg', 'Cadastrado realizado com sucesso!')
-        res.redirect('/usuario/login')
-      });
+      req.flash("success_msg", "Cadastrado realizado com sucesso!");
+      res.redirect("/usuario/login");
     } catch (erro) {
       console.error("Erro ao cadastrar:", erro);
       res.status(500).send("Erro ao cadastrar.");
