a0a716ef669fb507f7776000f66cdc676e4f87ab
Savio Henrique <saviobarradas@gmail.com>
2025-05-16 17:03:14 -0300

    estilização da pagina home

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,10 +3,14 @@ const router = express.Router();
 import UsuarioController from "../controllers/UsuarioController.js";
 import { logado } from "../config/regras.js";
 
-router.get("/login", (req, res) => res.render("usuario/login"));
+router.get("/login", (req, res) => res.render("usuario/login", {
+    header: 'header-login-cadastro'
+}));
 router.post("/login", UsuarioController.login);
 router.get("/logout", UsuarioController.logout);
-router.get("/cadastrar", (req, res) => res.render("usuario/cadastrar"));
+    router.get("/cadastrar", (req, res) => res.render("usuario/cadastrar", {
+        header: 'header-login-cadastro'
+    }));
 router.post("/cadastrar", UsuarioController.cadastrar);
 
 export default router;
